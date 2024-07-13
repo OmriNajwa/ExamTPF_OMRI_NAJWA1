d1b52ba68035ab27d9cb385e2ae52f368bce5273
najoi.omri@gmail.com <najoi.omri@gmail.com>
2024-07-13 14:20:45 +0100

    merge brache avec squrch

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ def somme(L):
     my_list=[1,5,7]
     som=somme(my_list)
     print(la somme est =,som)
+
+
+' my_list=[1,5,7]
+print('la somme est :',sum(my_list))
+print le max est :',max(my_list) print le min est :',min(my_list) '>> main.py
+
+my_list=[1,5,7]
+print('la somme est :',sum(my_list))
+print le max est :',max(my_list) print le min est :',min(my_list) '>> main.py
+else:
+    print('Liste vide')
+
+
